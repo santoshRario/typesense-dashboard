bac5b652ac24f90e86d32405b55e844a9fea6cbd
santoshRario <santosh.punase@rario.com>
2023-12-22 19:48:44 +0530

    added start script and public path

--- quasar.conf.js
+++ quasar.conf.js
@@ -56,7 +56,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      publicPath: process.env.PUBLIC_PATH ?? (process.env.NODE_ENV === 'development' ? '/' : '/typesense-dashboard'),
+      // publicPath: process.env.PUBLIC_PATH ?? (process.env.NODE_ENV === 'development' ? '/' : '/typesense-dashboard'),
       publicPath: process.env.PUBLIC_PATH ?? '/',
       // transpile: false,
 
