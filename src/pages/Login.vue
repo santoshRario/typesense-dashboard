99f81d57dfc4dc85ae2558360f2c33505fa7743b
santoshRario <santosh.punase@rario.com>
2023-12-27 11:06:01 +0530

    changed login ui

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -6,39 +6,14 @@
       </div>
       <div>
         <q-card bordered class="q-pa-lg shadow-1">
-          <q-card-section>
-            <q-form class="q-gutter-md">
-              <q-input
-                filled
-                v-model="apiKey"
-                type="password"
-                label="Api Key"
-              />
-              <p v-if="$q.platform.is.electron">
-                requires server with cors enabled only for search function.
-              </p>
-              <p v-else>requires server with cors enabled.</p>
-              <q-select
-                filled
-                v-model="node.protocol"
-                :options="protocolOptions"
-                label="Protocol"
-              />
-              <q-input filled v-model="node.host" type="text" label="host" />
-              <q-input filled v-model="node.port" type="number" label="port" />
-              <q-input filled v-model="node.path" type="text" label="path" hint="optional: leave blank or start with / and end without /"/>
-              <div class="text-left">
-                <q-toggle
-                  v-if="$q.platform.is.electron && node.protocol === 'https'"
-                  label="Check TLS"
-                  v-model="node.tls"
-                />
-              </div>
-            </q-form>
-          </q-card-section>
           <q-card-section v-if="error">
             <p class="text-red">{{ error }}</p>
           </q-card-section>
+
+          <q-card-section>
+            <p class="text-red">Host: {{ process.env.TS_HOST }}</p>
+          </q-card-section>
+
           <q-card-actions class="q-px-md row">
             <q-btn
               unelevated
@@ -48,11 +23,6 @@
               label="Login"
               @click="login()"
             />
-            <q-btn color="primary" size="lg" icon="sym_s_history">
-              <q-menu>
-                <server-history></server-history>
-              </q-menu>
-            </q-btn>
           </q-card-actions>
         </q-card>
       </div>
@@ -63,24 +33,20 @@
 <script lang="ts">
 import { NodeLoginDataInterface } from 'src/store/node/state';
 import { defineComponent } from 'vue';
-import ServerHistory from 'components/ServerHistory.vue';
 
 export default defineComponent({
   name: 'Login',
-  components: {
-    ServerHistory,
-  },
   data() {
     return {
-      apiKey: '',
+      apiKey: 'xyz',
       node: {
-        host: 'localhost',
-        port: '8108',
-        protocol: 'http',
+        email: 's@gmail.com',
+        host: 'typesense.staging.rario.com',
+        port: '443',
+        protocol: 'https',
         path: '',
         tls: true,
       },
-      protocolOptions: ['http', 'https'],
     };
   },
   computed: {
@@ -99,9 +65,6 @@ export default defineComponent({
         apiKey: this.apiKey,
         node: this.node,
       });
-    },
-    loginWithHistory(h: NodeLoginDataInterface) {
-      void this.$store.dispatch('node/login', h);
     },
   },
 });
